7b94f026fe1f8ac45fcfaf55805f1785bae6e534
rishitha2004 <rishithachowdarykonanki@gmail.com>
2023-06-05 17:34:54 +0530

    Created a button to erase all the previous entries.

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function save(){
     let toSave=cnt+"  -  ";
     entr.innerText+=toSave;
 }
+
+function erase(){
+    console.log("Clear pressed");
+    document.getElementById("count").innerHTML=0;
+    document.getElementById("previous").innerHTML="Previous entries:";
+}
